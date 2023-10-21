f4bfc237fe3a97885d4d857ca877eae956d3b199
MrBisquit <william@wtdawson.info>
2023-10-21 10:22:04 +0100

    Made some small changes to Window.Window.cs

--- Banana_OS_Basic_V2/Window/Window.cs
+++ Banana_OS_Basic_V2/Window/Window.cs
@@ -10,21 +10,29 @@ namespace Banana_OS_Basic_V2.Window
 {
     public class Window
     {
+        // Private variables that only functions in this class can edit
+        static bool _IsVisible { get; set; } = true;
+        static string _WindowName { get; set; } = "";
+        static Process.Process _Process { get; set; }
+        static bool _CanClose { get; set; } = true;
+        static WindowState _WindowState { get; set; } = WindowState.Normal;
+
         public bool CanMinimize = true;
         public bool CanMaximize = true;
         // System_Window only property
-        public bool CanClose { get; } = true;
+        public bool CanClose { get; } = _CanClose;
 
         public WindowType WindowType;
-        public ResizeMode ResizeMode = ResizeMode.CanResize;
-        public WindowState WindowState { get; } = WindowState.Normal;
+        public ResizeMode ResizeMode { get; set; } = ResizeMode.CanResize;
+        public WindowState WindowState { get; } = _WindowState;
+        public bool IsVisible { get; } = _IsVisible;
 
         // For task managing
-        public string WindowName { get; }
+        public string WindowName { get; } = _WindowName;
         // To be displayed to the user
         public string WindowTitle { get; set; }
         public bool AlwaysOnTop { get; set; }
-        public Process.Process Process { get; }
+        public Process.Process Process { get; } = _Process;
         public Process.Process[] SubProcesses { get; set; }
         public TitleBarProperties TitleBarProperties { get; set; } = new TitleBarProperties();
         public UI.UIElement[] Elements { get; set; }
@@ -42,17 +50,22 @@ namespace Banana_OS_Basic_V2.Window
 
         public void Show()
         {
-
+            _IsVisible = true;
         }
 
         public void Hide()
         {
-
+            _IsVisible = false;
         }
 
         public void Close()
         {
 
+        }
+
+        public void SetWindowState(WindowState state)
+        {
+            _WindowState = state;
         }
 
         public void RenderWindow(Canvas canvas, Kernel kernel)
